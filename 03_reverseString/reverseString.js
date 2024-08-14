cb9fc9c7599eef95e6e322dd4731a7d03bca999e
Pavle <pavlemilenkovic@users.noreply.github.com>
2024-08-14 12:45:56 +0200

    Ex.3 Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let revString = ""
+    for(let i = 0; i < string.length; i++) {
+        let c = string.charAt(string.length - 1 - i)
+        revString += c;
+    }
+    return revString;
 };
 
 // Do not edit below this line
