d11dce0b4ecdff72fd0e21b5d6d151dabd817295
Pavle <pavlemilenkovic@users.noreply.github.com>
2024-08-14 12:36:55 +0200

    Ex.2 Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    
+    let rS = "";
+    for (let i = 0; i < num; i++) {
+        rS += string;
+    }
 
+    return rS;
 };
 
 // Do not edit below this line
